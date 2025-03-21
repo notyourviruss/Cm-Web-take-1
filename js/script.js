ef8d93d041953c2d016500b764140de254c262de
notyourviruss <notyourapple1210o0@gmail.com>
2025-03-22 00:17:14 +0500

    updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 function showmega(event) {
     const getmega = document.getElementById('mega-menu');
     const getheader = document.getElementById('header');
+    const gethitbox = document.getElementById('course-button');
     if(event.type === "mouseover"){   
         getmega.classList.add('show')
         getheader.classList.add('mega-menu-shown')
+        gethitbox.classList.add('course-button')
     }
     else if(event.type === "mouseleave"){
         getmega.classList.remove('show')
-        
         getheader.classList.remove('mega-menu-shown')
+        gethitbox.classList.remove('course-button')
     }
 }
