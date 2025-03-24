36392830fdf959de1a8284cbd680e4a8363b08a7
Ahmed <notyourapple1210o0@gmail.com>
2025-03-24 16:55:17 +0500

    header

--- js/script.js
+++ js/script.js
@@ -13,3 +13,8 @@ function showmega(event) {
         gethitbox.classList.remove('course-button')
     }
 }
+
+fetch('header.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('header-placeholder').innerHTML = data)
+        .catch(error => console.error('Error loading header:', error));
