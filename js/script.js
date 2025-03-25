abbaf36e51edf6ad53dea638196efb5a65de3208
Ahmed <notyourapple1210o0@gmail.com>
2025-03-25 16:43:23 +0500

    header-fix

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function showmega(event) {
     }
 }
 
-fetch('header.html')
+fetch('components/header.html')
         .then(response => response.text())
         .then(data => document.getElementById('header-placeholder').innerHTML = data)
         .catch(error => console.error('Error loading header:', error));
